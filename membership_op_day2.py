df98e8f02df59198bb284015d6e017e98d1decc5
ashish222148 <ashish222148@gmail.com>
2024-04-13 09:01:15 +0530

    Day2 program of KR Network -Membership operator (in/not in)

---
+++ membership_op_day2.py
@@ -0,0 +1,9 @@
+print("Jai ho Guru Maharaj Premanand ji ")
+x=["amit","arun","Harsh","Yash","Pankhu"]
+for name in x:
+    print(name)
+y="Yashdeep"
+print("deeps" in y)
+item=29
+list_1=[1,2,3,4,5]
+print(item not in list_1)
