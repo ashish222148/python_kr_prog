3eec027dc7a978881be3f66e471c41ba183b7dec
ashish222148 <ashish222148@gmail.com>
2024-04-13 10:35:44 +0530

    How to use comma(single double triple

---
+++ datatypes_day2.py
@@ -0,0 +1,8 @@
+#pythin recomd that character or word should be within the single quote while line should be double quote and multiple line could be in triple quote
+x='M'
+y='ashish'
+z="My Name is Ashish"
+n='''My name is ashish and
+villege is Ailiya'''
+print(f"{x}\n{y}\n{z}\n{n}")
+
