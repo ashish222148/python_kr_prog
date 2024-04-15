feae578d79bc43b9bede718fca9e4350a733ebea
ashish222148 <ashish222148@gmail.com>
2024-04-15 11:17:18 +0530

    this is day4 programming for break and continue.

---
+++ break_day4.py
@@ -0,0 +1,10 @@
+for x in range(5):
+    if x==3:
+        break
+    print(x)
+while True:
+    name=input("enter the username..")
+    if name=="root":
+        print("Welcome root")
+        break
+    
---
+++ while_loopp_day4.py
@@ -0,0 +1,5 @@
+name=None
+while name!='root':
+    print("You have entered wrong name.")
+    name=input("Please enter the valid username :: ").lower()
+
