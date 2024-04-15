5a458905f3780d61e8c5a656f5a7cf874d7eaeef
ashish222148 <ashish222148@gmail.com>
2024-04-15 15:58:14 +0530

    some more on day4

---
+++ add_number_day4.py
@@ -0,0 +1,5 @@
+number=int(input("enter the number:"))
+result=0
+for x in range(1,number+1):
+    result=x+result
+print(result)
---
+++ bigger_num_day4.py
@@ -0,0 +1,8 @@
+number1=int(input("enter the number1.."))
+number2=int(input("enter the number2.."))
+if number1>number2:
+    print(f"{number1} is bigger than {number2}")
+elif number1==number2:
+    print("both numbers are same")
+else:
+    print(f"{number2} is bigger than {number1}")
