ba8c31064c5f55083dc76cc8043fac42661cfe67
ashish222148 <ashish222148@gmail.com>
2024-04-27 10:12:10 +0530

    very important for moduling

---
+++ module_adding_env_var_day8.py
@@ -0,0 +1,9 @@
+#module: collection of related function,variable and classes
+#standard ,third party module, own created
+#1-working directory,2-lib folder,
+#to check the environment variable
+import sys
+# print(sys.path)
+#add environment variable
+sys.path.append(r'F:\powershell_scripting\powershell_scr_prod\powershell_programming')
+print(sys.path)
